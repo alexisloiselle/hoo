47d0cd8169cd5bb921dc29c3871d9c6900071792
Alexandre Gagnon-Lalonde <agl@t-b.ca>
2023-09-13 10:33:50 -0400

    tab bar sketch

--- hoo-frontend/App.tsx
+++ hoo-frontend/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import ProfileScreen from "./screens/Profile";
 import HomeScreen from "./screens/HomeScreen";
+import LeaderboardScreen from "./screens/Leaderboard";
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
