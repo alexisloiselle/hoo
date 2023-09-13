1c623413231d60ca5b3e85535715ec60f958e736
Alexandre Gagnon-Lalonde <agl@t-b.ca>
2023-09-13 12:02:02 -0400

    navigation basic

--- hoo-frontend/App.tsx
+++ hoo-frontend/App.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import ProfileScreen from "./screens/Profile";
 import HomeScreen from "./screens/HomeScreen";
 import LeaderboardScreen from "./screens/Leaderboard";
 import AuthenticationProvider from "./providers/AuthenticationProvider";
 import { NotificationsProvider } from "./providers/notifications";
 
-const Tab = createBottomTabNavigator();
+import OwlSvg from "./components/OwlSvg";
+import ProfileSvg from "./components/ProfileSvg";
+import LeaderboardSvg from "./components/LeadeboardSvg";
 
+const Tab = createBottomTabNavigator();
 const queryClient = new QueryClient();
+const CommonTabBarOptions = {
+  tabBarActiveTintColor: "red",
+  tabBarInactiveTintColor: "green",
+  tabBarActiveBackgroundColor: "blue",
+  tabBarInactiveBackgroundColor: "yellow",
+};
 
 const App = () => {
   return (
@@ -18,10 +27,39 @@ const App = () => {
       <AuthenticationProvider>
         <NotificationsProvider>
           <NavigationContainer>
-            <Tab.Navigator>
-              <Tab.Screen name="Home" component={HomeScreen} />
-              <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
-              <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Navigator initialRouteName="Home">
+              <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  ...CommonTabBarOptions,
+                  headerShown: false,
+                  tabBarLabel: "Home",
+                  tabBarIcon: ({ color }) => <OwlSvg color={color} size={25} />,
+                }}
+              />
+              <Tab.Screen
+                name="Leaderboard"
+                component={LeaderboardScreen}
+                options={{
+                  ...CommonTabBarOptions,
+                  tabBarLabel: "Home",
+                  tabBarIcon: ({ color }) => (
+                    <LeaderboardSvg color={color} size={25} />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                  ...CommonTabBarOptions,
+                  tabBarLabel: "Home",
+                  tabBarIcon: ({ color }) => (
+                    <ProfileSvg color={color} size={25} />
+                  ),
+                }}
+              />
             </Tab.Navigator>
           </NavigationContainer>
         </NotificationsProvider>
